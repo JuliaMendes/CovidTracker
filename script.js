b26afbb94aa204dc70b5754e6a261681a3261da7
JuliaMendes <juliadepaivams@gmail.com>
2021-04-08 11:23:51 -0300

    covid tracker done

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const main = document.getElementById('main')
 let nameCountry 
 
 async function getCountry(country) {
+    try{
         const {data} = await axios(APIURL + 'countries/' + country)
         createCountryCard(data)
+    } catch(err){
+        createErrorCard(nameCountry + ' was not found')
+}       
 }
 
 
@@ -15,16 +19,27 @@ function createCountryCard(country) {
 
     const cardHTML = `
         <div class="card" id="card">
-            <div class="country">
+            <div class="name-country">
                 <h2>${nameCountry}</h2>
             </div>
-            <div class="info">
-                <h3>Confirmed:${country.confirmed.value}</h3>
-                <h3>Recovered:${country.recovered.value}</h3>
-                <h3>Deaths:${country.deaths.value}</h3>
+            <div class="covid-info">
+                <h3>Confirmed: ${country.confirmed.value}</h3>
+                <h3>Recovered: ${country.recovered.value}</h3>
+                <h3>Deaths: ${country.deaths.value}</h3>
             </div>
+            <p>Last Update: ${country.lastUpdate.replace(/-/g, '/').replace(/T/, ' ').replace(/\..+/, '')}</p>
         </div>
     `
+    main.innerHTML = cardHTML
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="card">
+            <h3>${msg}</h3>
+        </div>
+    `
+
     main.innerHTML = cardHTML
 }
 
